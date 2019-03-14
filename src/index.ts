ce54a57b337e714cd7648df5f79feaa003d7c8c9
Markus Dittmann <dmarku@posteo.de>
2019-03-14 15:43:38 +0100

    parameterize stage creation

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("3d-view") as HTMLCanvasElement;
   const engine = new Engine(canvas, true);
   const scene = createScene(engine);
+  const stage = createStage({ width: 10, depth: 10, height: 10 }, scene);
 
   const sphere = MeshBuilder.CreateIcoSphere("sphere", { radius: 1 }, scene);
   sphere.position = new Vector3(0, 5, 0);
@@ -32,46 +33,75 @@ function createScene(engine: Engine): Scene {
     scene
   );
   const sun = new HemisphericLight("sun", new Vector3(1, 1, 0), scene);
-  createStage(scene);
 
   return scene;
 }
 
-function createStage(scene: Scene) {
+interface StageParameters {
+  width: number;
+  depth: number;
+  height: number;
+}
+
+function createStage(parameters: StageParameters, scene: Scene) {
+  const { width, depth, height } = parameters;
   const stage = new TransformNode("stage");
+  const right = -0.5 * width;
+  const left = 0.5 * width;
+  const bottom = 0;
+  const top = height;
+  const front = 0.5 * depth;
+  const back = -0.5 * depth;
 
   const ground = MeshBuilder.CreateGround(
     "ground",
-    { width: 10, height: 10 },
+    { width, height: depth },
     scene
   );
   ground.parent = stage;
 
   const beam = MeshBuilder.CreateCylinder(
     "beam",
-    { height: 10, diameter: 0.2 },
+    { height, diameter: 0.2 },
     scene
   );
   beam.setEnabled(false);
 
-  beam.createInstance("back right pillar").position = new Vector3(-5, 5, -5);
-  beam.createInstance("back left pillar").position = new Vector3(5, 5, -5);
-  beam.createInstance("front right pillar").position = new Vector3(-5, 5, 5);
-  beam.createInstance("front left pillar").position = new Vector3(5, 5, 5);
+  const backRight = beam.createInstance("back right pillar");
+  backRight.position = new Vector3(right, 5, back);
+  backRight.parent = stage;
 
-  const front = beam.createInstance("front beam");
-  front.rotate(Axis.Z, Math.PI / 2);
-  front.position = new Vector3(0, 10, 5);
+  const backLeft = beam.createInstance("back left pillar");
+  backLeft.position = new Vector3(left, 5, back);
+  backLeft.parent = stage;
 
-  const back = beam.createInstance("back beam");
-  back.rotate(Axis.Z, Math.PI / 2);
-  back.position = new Vector3(0, 10, -5);
+  const frontRight = beam.createInstance("front right pillar");
+  frontRight.position = new Vector3(right, 5, front);
+  frontRight.parent = stage;
 
-  const right = beam.createInstance("left beam");
-  right.rotate(Axis.X, Math.PI / 2);
-  right.position = new Vector3(-5, 10, 0);
+  const frontLeft = beam.createInstance("front left pillar");
+  frontLeft.position = new Vector3(left, 5, front);
+  frontLeft.parent = stage;
 
-  const left = beam.createInstance("left beam");
-  left.rotate(Axis.X, Math.PI / 2);
-  left.position = new Vector3(5, 10, 0);
+  const frontBeam = beam.createInstance("front beam");
+  frontBeam.rotate(Axis.Z, Math.PI / 2);
+  frontBeam.position = new Vector3(0, top, front);
+  frontBeam.parent = stage;
+
+  const backBeam = beam.createInstance("back beam");
+  backBeam.rotate(Axis.Z, Math.PI / 2);
+  backBeam.position = new Vector3(0, top, back);
+  backBeam.parent = stage;
+
+  const rightBeam = beam.createInstance("left beam");
+  rightBeam.rotate(Axis.X, Math.PI / 2);
+  rightBeam.position = new Vector3(right, top, 0);
+  rightBeam.parent = stage;
+
+  const leftBeam = beam.createInstance("left beam");
+  leftBeam.rotate(Axis.X, Math.PI / 2);
+  leftBeam.position = new Vector3(left, top, 0);
+  leftBeam.parent = stage;
+
+  return stage;
 }
